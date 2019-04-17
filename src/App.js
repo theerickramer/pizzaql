c881957e6efc4f433a25758fb28cce49ef05fcdc
Eric Kramer <theerickramer@gmail.com>
2019-04-17 14:25:25 -0400

    sets up Apollo w pizzaql

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { ApolloProvider, Query } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: ' https://core-graphql.dev.waldo.photos/pizza'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <Query
+          query={gql`
+            {
+              pizzaSizes {
+                maxToppings
+              }
+            }
+          `}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return data.pizzaSizes.map(({ maxToppings }) => (
+              <div key={maxToppings}>
+                <p>maxToppings: {maxToppings}</p>
+              </div>
+            ));
+          }}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
