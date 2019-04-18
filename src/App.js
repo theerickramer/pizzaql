a70933c2e1c12203039dc1909f0b2ff7714ce98d
Eric Kramer <theerickramer@gmail.com>
2019-04-18 15:48:52 -0400

    componentizes pizza sizes

--- src/App.js
+++ src/App.js
@@ -2,79 +2,54 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
 import { ApolloProvider, Query } from 'react-apollo';
+import PizzaSizes from './PizzaSizes';
+import styles from './App.module.css';
 
 const client = new ApolloClient({
   uri: ' https://core-graphql.dev.waldo.photos/pizza'
 });
 
 class App extends Component {
-  state = { selected: 'small' };
+  state = { selected: null, cart: [] };
+  toggleSelectedPizzaSize = (pizzaSize) => {
+    this.setState({ selected: pizzaSize });
+  }
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Query
-          query={gql`
-            {
-              pizzaSizes {
-                name
-                maxToppings
-                basePrice
-                toppings {
-                  topping {
-                    name
-                    price
+      <div className={styles.app}>
+        <ApolloProvider client={client}>
+          <Query
+            query={gql`
+              {
+                pizzaSizes {
+                  name
+                  maxToppings
+                  basePrice
+                  toppings {
+                    topping {
+                      name
+                      price
+                    }
+                    defaultSelected
                   }
-                  defaultSelected
                 }
               }
-            }
-          `}
-        >
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-            console.log(data)
-            return data.pizzaSizes.map(
-              (
-                { name, maxToppings, basePrice, toppings },
-                index
-              ) => (
-                <div key={index}>
-                  <input
-                    type="checkbox"
-                    checked={this.state.selected === name}
-                    onChange={() =>
-                      this.state.selected === name
-                        ? this.setState({ selected: null })
-                        : this.setState({ selected: name })
-                    }
-                  />
-                  <h3>{name}</h3>
-                  <p>
-                    {!maxToppings ? 'Unlimited' : maxToppings}{' '}
-                    Toppings
-                  </p>
-                  <p>Price: {basePrice}</p>
-                  {this.state.selected === name
-                    ? toppings.map((topping, index) => (
-                        <div key={index}>
-                          <input
-                            type="checkbox"
-                            checked={topping.defaultSelected}
-                          />
-                          {topping.topping.name}
-                          {' $'}
-                          {topping.topping.price.toFixed(2)}
-                        </div>
-                      ))
-                    : null}
-                    <hr />
-                </div>
-              )
-            );
-          }}
-        </Query>
-      </ApolloProvider>
+            `}
+          >
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error :(</p>;
+              return (
+                <PizzaSizes
+                  data={data}
+                  selected={this.state.selected}
+                  toggleSelected={this.toggleSelectedPizzaSize}
+                />
+              );
+            }}
+          </Query>
+        </ApolloProvider>
+      </div>
     );
   }
 }
---
+++ src/PizzaSizes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const PizzaSizes = ({ data, selected, toggleSelected }) => {
+  return data.pizzaSizes.map(
+    ({ name, maxToppings, basePrice, toppings }, index) => (
+      <div key={index}>
+        <input
+          checked={selected === name}
+          className={styles.pizzaSizeSelect}
+          type="checkbox"
+          onChange={() =>
+            selected === name
+              ? toggleSelected(null)
+              : toggleSelected(name)
+          }
+        />
+        <h3 className={styles.pizzaSize}>{name}</h3>
+        <p>{!maxToppings ? 'Unlimited' : maxToppings} Toppings</p>
+        <p>Price: {basePrice}</p>
+        {selected === name
+          ? toppings.map((topping, index) => (
+              <div key={index}>
+                <input type="checkbox" checked={topping.defaultSelected} />
+                {topping.topping.name}
+                {' $'}
+                {topping.topping.price.toFixed(2)}
+              </div>
+            ))
+          : null}
+        <hr />
+      </div>
+    )
+  );
+};
+
+export default PizzaSizes;
